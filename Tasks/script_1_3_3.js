28869f92f81ce1de8fa592368bb0ba3439c3d12c
ShiroKaifat <89072081+ShiroKaifat@users.noreply.github.com>
2021-08-18 13:44:14 +0300

    Update script_1_3_3.js

--- Tasks/script_1_3_3.js
+++ Tasks/script_1_3_3.js
@@ -4,14 +4,14 @@ let array = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12,
     754, 2345, 2, 8, 244, 2545, 6, 3, 9];
 console.log(array);
 testFunction = (arr, val) => {
-    let newArray = [];
+    let newArray = array.filter(value => value > val);
 
-    array.forEach((value, index) => {
+    /*array.forEach((value, index) => {
 
         if (value > val) {
             newArray.push(value);
         }
-    });
+    });*/
     return lengthFunction(newArray);
 }
 
