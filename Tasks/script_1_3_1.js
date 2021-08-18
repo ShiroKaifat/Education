6212a24bf8367728c3971764e49e57897e8d57f2
ShiroKaifat <89072081+ShiroKaifat@users.noreply.github.com>
2021-08-18 10:23:12 +0300

    Add files via upload

---
+++ Tasks/script_1_3_1.js
@@ -0,0 +1,12 @@
+function func(arr) {
+    let count = 0;
+
+    arr.forEach(value => {
+       count++;
+    });
+    return count;
+}
+const elements = [4, 6, 2, 7, 8, 4];
+console.log(elements);
+const result =func(elements);
+console.log(result);
