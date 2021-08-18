ee6d6d943828deaa3c5c97e11d659cf676a1d4e1
ShiroKaifat <89072081+ShiroKaifat@users.noreply.github.com>
2021-08-18 14:11:47 +0300

    Update script_1_3_7.js

--- Tasks/script_1_3_7.js
+++ Tasks/script_1_3_7.js
@@ -6,7 +6,7 @@ let array = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12,
     754, 2345, 2, 8, 244, 2545, 6, 3, 9];
 console.log(array);
 
-testFunction = (arr, val1, val2) => {
+/*testFunction = (arr, val1, val2) => {
 
     arr.forEach(value => {
 
@@ -15,7 +15,6 @@ testFunction = (arr, val1, val2) => {
         }
 
     });
-}
-
-const t = testFunction(array, 4, 2);
+}*/
+const t = array.filter(value => (value === 4 || value === 2));
 console.log(t);
