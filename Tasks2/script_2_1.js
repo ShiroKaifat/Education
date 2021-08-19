e03cf6a72eb7284cb3b6159b8b91f24c7ce998a3
ShiroKaifat <89072081+ShiroKaifat@users.noreply.github.com>
2021-08-19 15:57:57 +0300

    Add files via upload

---
+++ Tasks2/script_2_1.js
@@ -0,0 +1,15 @@
+//Отсортировать массив чисел по возрастанию и убыванию.
+
+function sortElemW(arr, dir) {
+
+}
+function sortElemL(arr, dir) {
+
+}
+
+const elements = [6, 43, -6, 3, 0, 5, 2, 7];
+const sortedArrayW = sortElemW(elements);
+const sortedArrayL = sortElemL(elements);
+console.log(sortedArrayW);
+console.log(sortedArrayL);
+
