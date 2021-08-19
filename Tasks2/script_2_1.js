5a45302b49d352b11065f0caacf1d457154708c5
ShiroKaifat <89072081+ShiroKaifat@users.noreply.github.com>
2021-08-19 16:16:44 +0300

    Update script_2_1.js

--- Tasks2/script_2_1.js
+++ Tasks2/script_2_1.js
@@ -1,15 +1,21 @@
-//Отсортировать массив чисел по возрастанию и убыванию.
+//Отсортировать массив чисел по возрастанию или убыванию.
 
-function sortElemW(arr, dir) {
-
+function sortElemW(arr) {
+    arr.sort(function (a, b) {
+        return b - a;
+    })
+    return arr;
 }
-function sortElemL(arr, dir) {
-
+function sortElemL(arr) {
+    arr.sort(function (a, b) {
+        return a - b;
+    })
+    return arr;
 }
 
 const elements = [6, 43, -6, 3, 0, 5, 2, 7];
 const sortedArrayW = sortElemW(elements);
-const sortedArrayL = sortElemL(elements);
 console.log(sortedArrayW);
+const sortedArrayL = sortElemL(elements);
 console.log(sortedArrayL);
 
