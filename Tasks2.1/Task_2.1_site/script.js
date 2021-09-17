5a2e32a2ee4d554501f70533e300e0f937e33087
ShiroKaifat <89072081+ShiroKaifat@users.noreply.github.com>
2021-09-17 13:19:34 +0300

    Delete Tasks2.1 directory

--- Tasks2.1/Task_2.1_site/script.js
+++
@@ -1,9 +0,0 @@
-function changeSite() {
-    document.getElementById('container').style.display = 'flex';
-    document.getElementById('body').style.overflowY = 'hidden';
-}
-
-function rechangeSite() {
-    document.getElementById('container').style.display = 'none';
-    document.getElementById('body').style.overflowY = 'scroll';
-}
