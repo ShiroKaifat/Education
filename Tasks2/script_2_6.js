fc45ead441d47f879c226c4e840245fa7efabce7
ShiroKaifat <89072081+ShiroKaifat@users.noreply.github.com>
2021-09-17 13:19:44 +0300

    Delete Tasks2 directory

--- Tasks2/script_2_6.js
+++
@@ -1,9 +0,0 @@
-//Удалить из массива значение с конкретным индексом.
-
-findIndex = (arr, val) => {
-    return arr.filter((n) => {return n !== arr[val]});
-}
-
-const array = [5, 3, 8, 2, 9, 4, 1, 7];
-const t = findIndex(array, 4);
-console.log(t);
